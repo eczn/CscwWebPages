7581e759dd48ae1aa765e6d59fdb3874322b8fdc
momo622 <475600133@qq.com>
2016-08-01 22:36:58 +0800

    this is a new css file

--- js/devgrp.js
+++ js/devgrp.js
@@ -1,32 +1,29 @@
-// function block1(java){
-//     var java =document.getElementById('java');
-//     var cxt = java.getContext("2d");
-//     cxt.fillStyle = "rgba(113,177,202,1)";
-//     cxt.fillRect(0,0,340,200); 
-// }
-// block1();
-
-// function block2(html5){
-//     var html5 = document.getElementById('html5');
-//     var cxt = html5.getContext("2d");
-//     cxt.fillStyle = "rgba(113,177,202,1)";
-//     cxt.fillRect(0,0,340,200);
-// }
-// block2();
-
-// function block3(ios){
-//     var ios = document.getElementById('ios');
-//     var cxt =ios.getContext("2d");
-//     cxt.fillStyle = "rgba(113,177,202,1)";
-
-// }
-
 var links=document.getElementsByTagName('canvas');
-for( var i=0; i<links.length;i++){
-        (function (value){  
-            var value =links[i];
-            var cxt = value.getContext("2d");
-            cxt.fillStyle = "rgba(113,177,202,1)";
-            cxt.fillRect(0, 0, 340, 200);  
-        }());               
+for( var i=0; i<links.length;i+=2){
+    (function (value){  
+        var value = links[i];
+        var cxt = value.getContext("2d");
+        cxt.fillStyle ="rgba(249,181,48,1)" ;
+        cxt.fillRect(0, 0, 340, 200);  
+    }());               
 }
+
+for( var i=1; i<links.length;i+=2){
+    (function (value){  
+        var value = links[i];
+        var cxt = value.getContext("2d");
+        cxt.fillStyle = "rgba(113,177,202,1)";
+        cxt.fillRect(0, 0, 340, 200);  
+    }());               
+}
+
+$(document).ready(function(){
+    $(".changecolor").mouseleave(function(){
+        $(".colorchange").fadeTo(800,1);
+    });
+    $(".colorchange").mouseenter(function(){
+        $(this).fadeTo(800,0);
+    });
+});
+
+
