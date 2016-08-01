ec2546553370e2d552c397ede0b6850e0a895dc8
eczn <1975845422@qq.com>
2016-08-02 01:52:23 +0800

    适应jP_circle.js 对循环条件做了改动（因为在index.html里面添加了一个canvas标签）

--- js/devgrp.js
+++ js/devgrp.js
@@ -1,12 +1,12 @@
 var links=document.getElementsByTagName('canvas');
-for( var i=0; i<links.length;i+=2){
+for( var i=1; i<links.length;i+=2){
     var value = links[i];
     var cxt = value.getContext("2d");
     cxt.fillStyle ="rgba(249,181,48,1)" ;
     cxt.fillRect(0, 0, 340, 200);  
 }
 
-for( var i=1; i<links.length;i+=2){
+for( var i=2; i<links.length;i+=2){
     var value = links[i];
     var cxt = value.getContext("2d");
     cxt.fillStyle = "rgba(113,177,202,1)";
