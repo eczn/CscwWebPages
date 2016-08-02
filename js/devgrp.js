3f2cb934326e744a83174a46eb9f511a4082f7d5
eczn <1975845422@qq.com>
2016-08-02 23:43:45 +0800

    rebuild because of new way of building this

--- js/devgrp.js
+++ js/devgrp.js
@@ -7,7 +7,7 @@ for( var i=0; i<links.length;i+=2){
     var value = links[i];
     var cxt = value.getContext("2d");
     cxt.fillStyle ="rgba(249,181,48,1)" ;
-    cxt.fillRect(0, 0, 340, 200);  
+    cxt.fillRect(0, 0, 340, 200);
 }
 
 for( var i=1; i<links.length;i+=2){
