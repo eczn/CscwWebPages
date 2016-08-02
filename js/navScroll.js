46aff3fc797d02bcae25af9a4757ac41352855d2
ChenPt <847136903@qq.com>
2016-08-02 17:11:35 +0800

    更改一些小细节

--- js/navScroll.js
+++ js/navScroll.js
@@ -2,6 +2,7 @@ function scroll(){
 
 	$(window).scroll(function(){
 		var top = $(window).scrollTop();
+		//当滚动条下拉95px后 导航栏变成fixed， 字体颜色logo颜色改变
 		if(top >= 95){
 			$("#header").addClass("header-fixed");
 			$("#nav").addClass("nav-fixed");
@@ -9,6 +10,7 @@ function scroll(){
 			$("#logo-black").removeClass("hide");
 			$("#nav a").css("color","#000");
 		}
+		//当滚动条距离顶部距离小于95，清除fixed样式。
 		if(top < 95){
 			$("#header").removeClass("header-fixed");
 			$("#nav").removeClass("nav-fixed");
@@ -18,7 +20,6 @@ function scroll(){
 			$("#header").css("z-index",5);
 		}
 		var height = $("#header").height();
-		// console.log(height);
 		$("#homeContainer,#introducedContainer,#projectContainer,#connectContainer").css("top",height);
 	});
 }
