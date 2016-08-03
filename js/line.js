491fb1f1190593e9da34d9540c263550d207a2e2
eczn <1975845422@qq.com>
2016-08-03 15:54:22 +0800

    canvas line rendering 由line.js强力驱动

--- js/line.js
+++ js/line.js
@@ -4,13 +4,31 @@
 function penInit(className,width,height){
 	var canvas = $("."+className); 
 	var i; 
+
 	for (i=0;i<canvas.length;i++){
 		canvas[i].width = width; 
 		canvas[i].height = height; 
 	}
 
+	this.render = function(){
+		var i = 0; 
 
+		for (i=0;i<canvas.length;i++){
+			var g = canvas[i].getContext('2d');
+			g.beginPath();
+			g.strokeStyle = "#FFF";
+			g.lineWidth = 3;
+			g.moveTo(width,30);
+			g.lineTo(width/2,30);
+			g.lineTo(0,80);
+			g.stroke();
+			g.closePath();
+
+		}
+
+	}
 
 }
 
-penInit("canvas-line",80,150); 
+var pen = new penInit("canvas-line",80,150);
+pen.render();
