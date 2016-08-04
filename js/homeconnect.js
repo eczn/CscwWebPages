383045d40ff2ac1a1215301dcbe0224845523eb5
ruiwenruiwen <493143643@qq.com>
2016-08-05 02:03:14 +0800

    connect节奏修改

--- js/homeconnect.js
+++ js/homeconnect.js
@@ -2,15 +2,15 @@ $(document).ready(function(){
 
 	// var left = $("#connect").css("left");
 
-	$(".border,.mainIntro,.intromore").animate({"opacity":"0","margin-top":"+=70px"},0);
 	$(".yellow").animate({"opacity":"0","bottom":"-50px"},0);
-	$(".border,.mainIntro,.intromore").delay(1000).animate({"opacity":1,"margin-top":"-=70px"});
+	$(".border,.mainIntro,.intromore").animate({"opacity":"0","margin-top":"+=70px"},0);
+	$(".border,.mainIntro,.intromore").delay(900).animate({"opacity":1,"margin-top":"-=70px"});
 	$(".yellow").delay(800).animate({"opacity":"1","bottom":"0"});
 
 	$("#connect").on("click",function(){
-		if(!$(".phone,.address,.email").is(":animated")&&($(".active")[0].id!="connect")){
+		if(!$(".phone,.address,.email").is(":animated") && ($(".active")[0].id!="connect")){
 			$(".phone,.address,.email").animate({"opacity":"0","left":"+=7%"},0);
-			$(".phone,.address,.email").delay(1000).animate({"opacity":1,"left":"-=7%"});
+			$(".phone,.address,.email").delay(900).animate({"opacity":1,"left":"-=7%"});
 		}
 	})
 
@@ -19,7 +19,7 @@ $(document).ready(function(){
 	// }
 
 	$("#home").on("click",function(){
-		if(!$(".border,.mainIntro,.intromore,.yellow").is(":animated")&&($(".active")[0].id!="home")){
+		if(!$(".border,.mainIntro,.intromore,.yellow").is(":animated") && ($(".active")[0].id!="home")){
 			$(".border,.mainIntro,.intromore").animate({"opacity":"0","margin-top":"+=70px"},0);
 			$(".yellow").animate({"opacity":"0","bottom":"-50px"});
 			$(".border,.mainIntro,.intromore").delay(1000).animate({"opacity":1,"margin-top":"-=70px"});
