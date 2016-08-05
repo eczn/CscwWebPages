cc4237e42b18636b832d00270edab9bde4110617
ruiwenruiwen <493143643@qq.com>
2016-08-05 10:11:25 +0800

    js of home & connect

--- js/homeconnect.js
+++ js/homeconnect.js
@@ -1,29 +1,21 @@
 $(document).ready(function(){
 
-	// var left = $("#connect").css("left");
-
-	$(".yellow").animate({"opacity":"0","bottom":"-50px"},0);
+	$(".yellow").animate({"opacity":"0"},0);
 	$(".border,.mainIntro,.intromore").animate({"opacity":"0","margin-top":"+=70px"},0);
-	$(".border,.mainIntro,.intromore").delay(900).animate({"opacity":1,"margin-top":"-=70px"});
-	$(".yellow").delay(800).animate({"opacity":"1","bottom":"0"});
+	$(".border,.mainIntro,.intromore").delay(300).animate({"opacity":1,"margin-top":"-=70px"});
+	$(".yellow").delay(100).animate({"opacity":"1"});
 
 	$("#connect").on("click",function(){
 		if(!$(".phone,.address,.email").is(":animated") && ($(".active")[0].id!="connect")){
-			$(".phone,.address,.email").animate({"opacity":"0","left":"+=7%"},0);
-			$(".phone,.address,.email").delay(900).animate({"opacity":1,"left":"-=7%"});
+			$(".phone,.address,.email").animate({"opacity":0},0).delay(200).animate({"opacity":0.1},0);
+			$(".phone,.address,.email").delay(200).animate({"opacity":1},500);
 		}
 	})
 
-	// if(!$(".phone,.address,.email").is(":animated")&&($(".active")[0].id!="connect")&&(left=="30%")){
-	// 	$(".phone,.address,.email").animate({"opacity":"0"},0);
-	// }
-
 	$("#home").on("click",function(){
 		if(!$(".border,.mainIntro,.intromore,.yellow").is(":animated") && ($(".active")[0].id!="home")){
-			$(".border,.mainIntro,.intromore").animate({"opacity":"0","margin-top":"+=70px"},0);
-			$(".yellow").animate({"opacity":"0","bottom":"-50px"});
-			$(".border,.mainIntro,.intromore").delay(1000).animate({"opacity":1,"margin-top":"-=70px"});
-			$(".yellow").delay(800).animate({"opacity":"1","bottom":"0"});
+			$(".border,.mainIntro,.intromore").delay(1000).animate({"opacity":1,"margin-top":"-=70px"},500);
+			$(".yellow").delay(800).animate({"opacity":"1"},500);
 		}
 	})
 
--- js/navControl.js
+++ js/navControl.js
@@ -1,17 +1,39 @@
 
 function navInit(){
+	var memuStatus = 0; 
 	this.initActive = function(){
 		// 被点击的时候清空类名
 		//$(navBtn).removeClass("active"); 
 
-
 		var preview_active_name = $(".active")[0].id;
 		console.log("preview_active_name : "+ preview_active_name);
-
 		var now_active_name = $(this)[0].id;
 		console.log("now_active_name : " + now_active_name);
-
 		console.log($("#"+now_active_name).attr('position')); 
+
+		setTimeout(function(){
+			if (now_active_name == 'project'){
+				$(".animate-back").animate({
+					top: "100%",
+				},1000,function(){
+					$(this).css("display","none");
+					$(".animate-back-left").animate({
+						left: "-100%"
+					},1000,function(){
+						$(this).css("display","none");
+					});
+					$(".animate-back-right").animate({
+						right: "-100%"
+					},1000,function(){
+						$(this).css("display","none");
+					});
+				});
+			} else {
+				$(".animate-back").css("top","0").css("display","block"); 
+				$(".animate-back-left").css("left","0").css("display","block");;
+				$(".animate-back-right").css("right","0").css("display","block");;
+			}
+		},1000);
 
 		if (now_active_name == preview_active_name || $("#"+preview_active_name+"Container").is(":animated")){
 			return false; 
@@ -35,11 +57,30 @@ function navInit(){
 		// 给被点击的添加类名 
 		$(this).addClass("active");
 
-
-
 		$("#"+now_active_name+"Container").css("left",100*dx+"%").animate({
 			left: "0",
 		},1000);
 
+		if (preview_active_name=="home") {
+			$(".border,.mainIntro,.intromore").animate({"opacity":0,"margin-top":"+=70px"});
+			$(".yellow").animate({"opacity":"0"});
+		}
+
+		if (preview_active_name=="connect") {
+			$(".phone,.address,.email").animate({"opacity":"0"},600);
+		}
+	}
+	
+	this.mobileTouch = function(){
+		if (memuStatus == 0){
+			memuStatus = 1; 
+			$("#memu").css("display","block").fadeTo(600,1); 
+		} else if (memuStatus == 1){
+			memuStatus = 0;
+			$("#memu").fadeTo(600,0,function(){
+				$(this).css("display","none"); 
+			});
+		}
+		console.log("memu switch"); 
 	}
 }
